79a7fe9367141a822e2cf873532721daa439af98
Torch29 <ashtonerico@yahoo.co.jp>
2025-06-03 22:53:09 +0900

    勤怠一覧画面の基本構成配置、CSS仮適用

--- src/resources/views/staff/attendance/list.blade.php
+++ src/resources/views/staff/attendance/list.blade.php
@@ -1,9 +1,38 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/list.css') }}">
 @endsection
 
 @section('content')
-勤怠一覧
+<div class="list__content">
+    <div class="list__title">
+        <h3>勤怠一覧</h3>
+    </div>
+    <div class="list__select-area">
+        ←前月
+        当月
+        翌月→
+    </div>
+    <div class="list__content-table">
+        <table class=" list__table">
+            <tr class="list__table-row">
+                <th class="list__table-heading">日付</th>
+                <th class="list__table-heading">出勤</th>
+                <th class="list__table-heading">退勤</th>
+                <th class="list__table-heading">休憩</th>
+                <th class="list__table-heading">合計</th>
+                <th class=" list__table-heading">詳細</th>
+            </tr>
+            <tr class="list__table-row">
+                <td class="list__table-data">06/01（木）</td>
+                <td class="list__table-data">09:00</td>
+                <td class="list__table-data">18:00</td>
+                <td class="list__table-data">1:00</td>
+                <td class="list__table-data">8:00</td>
+                <td class="list__table-data">詳細</td>
+            </tr>
+        </table>
+    </div>
+</div>
 @endsection
