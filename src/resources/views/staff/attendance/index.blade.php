a94a1ce84bec8177c5e4e5a95d6650d35c170b19
Torch29 <ashtonerico@yahoo.co.jp>
2025-06-03 20:00:00 +0900

    index.css作成

--- src/resources/views/staff/attendance/index.blade.php
+++ src/resources/views/staff/attendance/index.blade.php
@@ -5,20 +5,22 @@
 @endsection
 
 @section('content')
-<div class="main__content">
-    <div class="status-label">勤務外</div>
-    <div class="main__content-date">
-        {{ \Carbon\Carbon::now()->isoFormat('Y/M/D（ddd）') }}
-    </div>
-    <div class="main__content-datetime">
-        {{ \Carbon\Carbon::now()->format('H:i') }}
-    </div>
-    <div class="attendance-form">
-        <form action="">
-            <div class="attendance-form__button">
-                <button class="attendance-form__button-submit">出勤</button>
-            </div>
-        </form>
+<div class="main__content-wrapper">
+    <div class="main__content">
+        <div class="status-label">勤務外</div>
+        <div class="main__content-date">
+            {{ \Carbon\Carbon::now()->isoFormat('Y/M/D（ddd）') }}
+        </div>
+        <div class="main__content-datetime">
+            {{ \Carbon\Carbon::now()->format('H:i') }}
+        </div>
+        <div class="attendance-form">
+            <form action="">
+                <div class="attendance-form__button">
+                    <button class="attendance-form__button-submit">出勤</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
