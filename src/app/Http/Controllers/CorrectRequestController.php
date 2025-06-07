bc17aa3b1b4549390b5a0df84cc36d02dca7f09a
Torch29 <ashtonerico@yahoo.co.jp>
2025-06-07 22:54:46 +0900

    勤怠登録画面の時刻表示にリアルタイムでの表示機能を追加

--- src/app/Http/Controllers/CorrectRequestController.php
+++ src/app/Http/Controllers/CorrectRequestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CorrectionRequestController extends Controller
+class CorrectRequestController extends Controller
 {
     public function index()
     {
--- src/resources/views/staff/attendance/index.blade.php
+++ src/resources/views/staff/attendance/index.blade.php
@@ -11,9 +11,24 @@
         <div class="main__content-date">
             {{ \Carbon\Carbon::now()->isoFormat('Y年 M月D日（ddd）') }}
         </div>
-        <div class="main__content-datetime">
-            {{ \Carbon\Carbon::now()->format('H:i') }}
-        </div>
+        <div class="main__content-datetime" id="clock"></div>
+        <script>
+            function nowTime() {
+                const timeSetting = {
+                    timeZone: 'Asia/Tokyo',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                }
+                const timeString = new Date().toLocaleTimeString('ja-JP', timeSetting);
+
+                document.getElementById("clock").textContent = timeString;
+            }
+
+            nowTime(); // 初回表示
+            setInterval(nowTime, 1000); // 毎秒更新
+        </script>
+
         <div class="attendance-form">
             <form action="">
                 <div class="attendance-form__button">
