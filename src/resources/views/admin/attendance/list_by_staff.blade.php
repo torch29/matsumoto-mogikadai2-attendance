a8d2047e0d2517c662cb5183f77f71ebd2424cfb
Torch29 <ashtonerico@yahoo.co.jp>
2025-06-14 12:37:02 +0900

    管理者アカウント用の「スタッフ別勤怠一覧画面」にて勤怠情報取得・表示機能追加

--- src/resources/views/admin/attendance/list_by_staff.blade.php
+++ src/resources/views/admin/attendance/list_by_staff.blade.php
@@ -30,8 +30,8 @@
                 <td class="attendance-table__data">{{ $attendance['date'] }}</td>
                 <td>{{ $attendance['clock_in'] }}</td>
                 <td class="attendance-table__data">{{ $attendance['clock_out'] }}</td>
-                <td class="attendance-table__data">1:00</td>
-                <td class="attendance-table__data">8:00</td>
+                <td class="attendance-table__data">{{ $attendance['total_rest_formatted'] }}</td>
+                <td class="attendance-table__data">{{ $attendance['total_work_formatted'] }}</td>
                 <td class="attendance-table__data">詳細</td>
             </tr>
             @endforeach
