ed6e99c787fe350e1e8ff4144e38727ca50e14b6
Torch29 <ashtonerico@yahoo.co.jp>
2025-06-25 18:25:55 +0900

    AttendanceCorrectionRequestファイルの作成

---
+++ src/app/Http/Requests/AttendanceCorrectionRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AttendanceCorrectionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'corrected_clock_in' => 'required' | 'date_format:H:i',
+            'corrected_clock_out' => 'required' | 'date_format:H:i' | 'after:corrected_clock_in',
+            'note' => 'required',
+
+            'rest_corrections' => 'nullable' | 'array',
+
+            'rest_corrections.*.corrected_rest_start' => 'nullable' | 'date_format:H:i' | 'after_or_equal:corrected_clock_in' | 'before:corrected_clock_out',
+
+            'rest_corrections.*.corrected_rest_end' => 'nullable' | 'date_format:H:i' | 'after:corrected_rest_start' | 'before:corrected_clock_out',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'corrected_clock_in.required' => '出勤時間を入力してください。',
+            'corrected_clock_out.required' => '退勤時間を入力してください。',
+            'corrected_clock_in.date_format' => '半角数字で X:XX 形式で入力してください。',
+            'corrected_clock_out.date_format' => '半角数字で X:XX 形式で入力してください。',
+            'corrected_clock_out.after' => '出勤時間もしくは退勤時間が不適切な値です',
+            'note.required' => '備考を記入してください',
+
+            'rest_corrections.*.corrected_rest_start.date_format' => '半角数字で X:XX 形式で入力してください。',
+            'rest_corrections.*.corrected_rest_end.date_format' => '半角数字で X:XX 形式で入力してください。',
+            'rest_corrections.*.corrected_rest_start.after_or_equal' => '休憩時間が勤務時間外です',
+            'rest_corrections.*.corrected_rest_start.before' => '休憩時間が勤務時間外です',
+            'rest_corrections.*.corrected_rest_end.after' => '休憩時間が勤務時間外です',
+            'rest_corrections.*.corrected_rest_end.before' => '休憩時間が勤務時間外です',
+        ];
+    }
+}
