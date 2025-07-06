d08e3b0d3f9a3dc2832db60c8c15c2cdb0500ef8
Torch29 <ashtonerico@yahoo.co.jp>
2025-07-06 17:16:17 +0900

    AdminAttendanceListTestにテスト記述

--- src/tests/Feature/AdminAttendanceListTest.php
+++ src/tests/Feature/AdminAttendanceListTest.php
@@ -21,17 +21,20 @@ class AdminAttendanceListTest extends TestCase
     //管理者は、勤怠一覧画面からその日になされた全ユーザーの勤怠情報を確認できる
     public function test_admin_can_check_all_attendance_data_for_staff()
     {
+        //職員を２人作成し、２人の勤怠情報を登録する
         $user1 = User::factory()->create([
             'name' => 'テスト　職員１'
         ]);
         $user2 = User::factory()->create([
             'name' => 'サンプル　職員２'
         ]);
+        //user1は出勤と退勤の打刻がある
         $attendance1 = $user1->attendances()->create([
             'date' => Carbon::now()->toDateString(),
             'clock_in' => Carbon::parse('07:50'),
             'clock_out' => Carbon::parse('17:45'),
         ]);
+        //user2は出勤と休憩入・休憩戻の打刻があり、まだ退勤していない
         $attendance2 = $user2->attendances()->create([
             'date' => Carbon::now()->toDateString(),
             'clock_in' => Carbon::parse('08:05'),
@@ -74,5 +77,69 @@ class AdminAttendanceListTest extends TestCase
         $response = $this->get('/admin/attendance/list');
         $response->assertViewIs('admin.attendance.list_all');
         $response->assertSee(now()->isoFormat('Y年M月D日'));
+    }
+
+    //「前日」を押下したときに前日の勤怠情報が表示される
+    public function test_show_previous_day_when_click_previous_day_link_at_all_attendance_list_for_admin()
+    {
+        //前日の勤怠情報を作成
+        $user = User::factory()->create();
+        $previousDay = Carbon::now()->subDay(1)->toDateString();
+        $attendance = $user->attendances()->create([
+            'user_id' => $user->id,
+            'date' => $previousDay,
+            'clock_in' => Carbon::parse('08:00'),
+            'clock_out' => Carbon::parse('18:00'),
+        ]);
+
+        //管理者としてログイン
+        $admin = User::factory()->create([
+            'is_admin' => 1,
+        ]);
+        $this->actingAs($admin);
+
+        //管理者用勤怠一覧画面から前日リンクをクリックし、前日の日付と前日に登録された勤怠情報が表示されていることを確認
+        $response = $this->get('/admin/attendance/list');
+        $response = $this->get('/admin/attendance/list?date=' . $previousDay);
+        $response->assertSeeInOrder(
+            [
+                Carbon::parse($previousDay)->isoFormat('Y年M月D日'),
+                $attendance->clock_in->format('H:i'),
+                $attendance->clock_out->format('H:i'),
+                $attendance->total_work_formatted,
+            ]
+        );
+    }
+
+    //「翌日」を押下したときに翌日の勤怠一覧が表示される
+    public function test_show_next_day_when_click_next_day_link_at_all_attendance_list_for_admin()
+    {
+        //当日の勤怠情報を作成
+        $user = User::factory()->create();
+        $nextDay = Carbon::now()->addDay(1)->toDateString();
+        $attendance = $user->attendances()->create([
+            'user_id' => $user->id,
+            'date' => now()->toDateString(), //当日の勤怠情報を作成
+            'clock_in' => Carbon::parse('08:00'),
+            'clock_out' => Carbon::parse('18:00'),
+        ]);
+
+        //管理者としてログイン
+        $admin = User::factory()->create([
+            'is_admin' => 1,
+        ]);
+        $this->actingAs($admin);
+
+        //管理者用勤怠一覧画面から翌日のリンクをクリックし、勤怠情報が表示されておらず、翌日の日付が表示されている
+        $response = $this->get('/admin/attendance/list');
+        $response = $this->get('/admin/attendance/list?date=' . $nextDay);
+        $response->assertSee(Carbon::parse($nextDay)->isoFormat('Y年M月D日'));
+        $response->assertDontSee(
+            [
+                $attendance->clock_in->format('H:i'),
+                $attendance->clock_out->format('H:i'),
+                $attendance->total_work_formatted,
+            ]
+        );
     }
 }
