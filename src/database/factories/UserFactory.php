b47d6e1b234ad742b7a130cd9aa29771a4254034
Torch29 <ashtonerico@yahoo.co.jp>
2025-07-14 18:39:44 +0900

    UserFactoryの微修正

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('12345678'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
