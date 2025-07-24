9378b978f84592ac0d42ec0f16c22c65fd6145f0
Torch29 <ashtonerico@yahoo.co.jp>
2025-07-24 18:52:01 +0900

    AppServiceProviderにCarbonの地域設定追記

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Carbon::setLocale('ja');
     }
 }
