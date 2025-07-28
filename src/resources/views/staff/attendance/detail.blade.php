4acfff8fd2611629da0a6209589fb4e5b7ffd2b3
Torch29 <ashtonerico@yahoo.co.jp>
2025-07-28 16:28:21 +0900

    staff用detail.blade.phpのクラス名修正とcss反映

--- src/resources/views/staff/attendance/detail.blade.php
+++ src/resources/views/staff/attendance/detail.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<div class="detail__content">
+<div class="detail">
     @if (session('error'))
     <div class="item__alert">
         <input type="checkbox" id="alert-close" class="alert-close">
@@ -20,28 +20,28 @@
         <h3>勤怠詳細</h3>
     </div>
     @if ( $latestCorrection && $latestCorrection->approve_status === 'pending' )
-    <div class="approve__status-message">下記のデータにて申請しました。承認をお待ちください。</div>
+    <div class="approve__status-message--pending">下記のデータにて申請しました。承認をお待ちください。</div>
     @endif
     <div class="detail-table__wrapper">
-        <form action="/correction_request" class="detail-form" method="post">
+        <form action="/correction_request" class="detail__form" method="post">
             @csrf
-            <table class="detail-table">
-                <tr class="detail-table__row">
-                    <th class="detail-table__heading">名前</th>
+            <table class="detail-form__table">
+                <tr>
+                    <th>名前</th>
                     <td class="detail-table__data-left" colspan="2"><span>{{ $attendance->user->name }}</span></td>
                 </tr>
-                <tr class="detail-table__row">
-                    <th class="detail-table__heading">日付</th>
+                <tr>
+                    <th>日付</th>
                     <td class="detail-table__data-left"><span>{{ $attendance->date->isoFormat('Y') }}年</span></td>
-                    <td class="detail-table__data"><span>{{ $attendance->date->isoFormat('M') . '月' . $attendance->date->isoFormat('D') . '日'  }}</span></td>
+                    <td><span>{{ $attendance->date->isoFormat('M') . '月' . $attendance->date->isoFormat('D') . '日'  }}</span></td>
                 </tr>
-                <tr class="detail-table__row">
-                    <th class="detail-table__heading">出勤・退勤</th>
+                <tr>
+                    <th>出勤・退勤</th>
                     <td class="detail-table__data-left">
-                        <input type="time" class="detail-table__input" name="corrected_clock_in" value="{{ old("corrected_clock_in", optional($displayClockIn)->format('H:i')) }}">　～
+                        <input type="time" class="detail-form__input" name="corrected_clock_in" value="{{ old("corrected_clock_in", optional($displayClockIn)->format('H:i')) }}">　～
                     </td>
-                    <td class="detail-table__data">
-                        <input type="time" class="detail-table__input" name="corrected_clock_out" max="23:59" value="{{ old("corrected_clock_out", optional($displayClockOut)->format('H:i')) }}">
+                    <td>
+                        <input type="time" class="detail-form__input" name="corrected_clock_out" max="23:59" value="{{ old("corrected_clock_out", optional($displayClockOut)->format('H:i')) }}">
                     </td>
                     <td class="table__data--error">
                         <p class="error__message">
@@ -57,15 +57,15 @@
                     </td>
                 </tr>
                 @foreach( $restRecords as $i => $rest )
-                <tr class="detail-table__row">
-                    <th class="detail-table__heading">
+                <tr>
+                    <th>
                         {{ $i === 0 ? '休憩' : '休憩' . ($i + 1) }}
                     </th>
                     <td class="detail-table__data-left">
-                        <input type="time" class="detail-table__input" name="rest_corrections[{{ $i }}][corrected_rest_start]" value="{{ old("rest_corrections.$i.corrected_rest_start", $rest->rest_start->format('H:i')) }}">　～
+                        <input type="time" class="detail-form__input" name="rest_corrections[{{ $i }}][corrected_rest_start]" value="{{ old("rest_corrections.$i.corrected_rest_start", $rest->rest_start->format('H:i')) }}">　～
                     </td>
-                    <td class="detail-table__data">
-                        <input type="time" class="detail-table__input" name="rest_corrections[{{ $i }}][corrected_rest_end]" value="{{ old("rest_corrections.$i.corrected_rest_end", optional($rest->rest_end)->format('H:i')) }}">
+                    <td>
+                        <input type="time" class="detail-form__input" name="rest_corrections[{{ $i }}][corrected_rest_end]" value="{{ old("rest_corrections.$i.corrected_rest_end", optional($rest->rest_end)->format('H:i')) }}">
                     </td>
                     <td class="table__data--error">
                         <p class="error__message">
@@ -81,15 +81,15 @@
                     </td>
                 </tr>
                 @endforeach
-                <tr class="detail-table__row">
-                    <th class="detail-table__heading">
+                <tr>
+                    <th>
                         {{ count($restRecords) === 0 ? '休憩' : '休憩' . (count($restRecords) + 1) }}
                     </th>
                     <td class="detail-table__data-left">
-                        <input type="time" class="detail-table__input" name="rest_corrections[new][corrected_rest_start]" value="{{ old("rest_corrections.new.corrected_rest_start") }}">　～
+                        <input type="time" class="detail-form__input" name="rest_corrections[new][corrected_rest_start]" value="{{ old("rest_corrections.new.corrected_rest_start") }}">　～
                     </td>
-                    <td class="detail-table__data">
-                        <input type="time" class="detail-table__input" name="rest_corrections[new][corrected_rest_end]" value="{{ old("rest_corrections.new.corrected_rest_end") }}">
+                    <td>
+                        <input type="time" class="detail-form__input" name="rest_corrections[new][corrected_rest_end]" value="{{ old("rest_corrections.new.corrected_rest_end") }}">
                     </td>
                     <td class="table__data--error">
                         <p class="error__message">
@@ -104,10 +104,10 @@
                         </p>
                     </td>
                 </tr>
-                <tr class="detail-table__row">
-                    <th class="detail-table__heading">備考</th>
-                    <td class="detail-table__data" colspan="2">
-                        <textarea name="note" id="" class="detail-table__textarea" placeholder="必須入力です。入力例：電車遅延のため">{{ old('note', $displayNote) }}</textarea>
+                <tr>
+                    <th>備考</th>
+                    <td colspan="2">
+                        <textarea name="note" id="" class="detail-form__textarea" placeholder="必須入力です。入力例：電車遅延のため">{{ old('note', $displayNote) }}</textarea>
                     </td>
                     <td>
                         <p class="error__message">
